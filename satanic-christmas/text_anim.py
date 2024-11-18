f7571e070f74477c62e5464d918b24042e319c6c
Maxime Chevalier <maximechevalierb@gmail.com>
2024-11-17 21:54:34 -0500

    Noise fadeout animation

--- satanic-christmas/text_anim.py
+++ satanic-christmas/text_anim.py
@@ -12,19 +12,23 @@ cv2.namedWindow("image", cv2.WINDOW_NORMAL)
 # Make window fullscreen
 cv2.setWindowProperty("image", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
+# 1080p resolution, 16:9 aspect ration, like the projector
+width = 1920
+height = 1080
+
+# Frame buffer
+# In floating point format so we can fade out easily
+fb = np.zeros((height, width, 3), dtype=np.float32)
 
 
 
 
 
-while True:
 
-    # 1080p resolution, 16:9 aspect ration, like the projector
-    img = np.zeros((1080, 1920, 3), dtype=np.uint8)
+def draw_text(width, height):
+    img = np.zeros((height, width, 3), dtype=np.uint8)
 
-
-
-    font.putText(img=img,
+    font.putText(img,
         text='Merry Christmas',
         org=(240, 300),
         fontHeight=250,
@@ -34,14 +38,18 @@ while True:
         bottomLeftOrigin=False,
     )
 
+    return img.astype(np.float32) / 255
 
 
 
+def draw_noise_line(img):
+    p1 = np.random.randint(low=(0, 0), high=(width, height), size=2, dtype=int)
+    p2 = np.random.randint(low=(0, 0), high=(width, height), size=2, dtype=int)
 
     cv2.line(
         img,
-        (0, 0),
-        (700, 700),
+        p1,
+        p2,
         color=(0, 0, 0),
         lineType=cv2.LINE_AA,
         thickness=1
@@ -49,8 +57,28 @@ while True:
 
 
 
+text = draw_text(width, height)
+
+
+print(text.dtype)
+
+
+fb += text
+
+
+
+while True:
+
+
+
+    for i in range(10):
+        draw_noise_line(fb)
+
+    fb *= 0.999
+
+
     # Show the current frame
-    cv2.imshow('image', img) 
+    cv2.imshow('image', fb) 
 
 
 
